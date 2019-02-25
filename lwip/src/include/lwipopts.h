b151f10bcc4086c147a181e9fefb769e0f900d2a
Dave Rush <northwoodlogic.free@gmail.com>
2019-02-25 15:14:34 -0600

    import lwip-2.1.2

---
+++ lwip/src/include/lwipopts.h
@@ -0,0 +1,22 @@
+#ifndef LWIP_OPTS_H
+#define LWIP_OPTS_H
+
+#define NO_SYS 1
+
+// Sockets API won't work without threads.
+#define LWIP_SOCKET 0
+#define LWIP_NETCONN 0
+
+// May want to look into these
+// #define MEM_LIBC_MALLOC 1
+// #define MEMP_MEM_MALLOC 1
+// #define MEMP_MEM_INIT 1
+#define MEM_ALIGNMENT 4
+
+// U-Boot gives these to us.
+#define LWIP_DONT_PROVIDE_BYTEORDER_FUNCTIONS 1
+
+// We've only got one thread and one context
+#define SYS_LIGHTWEIGHT_PROT 0
+
+#endif
---
+++ lwip/src/include/string.h
@@ -0,0 +1,11 @@
+#ifndef _UB_LW_STRING_GLUE_H
+#define _UB_LW_STRING_GLUE_H
+
+
+#ifndef USE_HOSTCC
+#include <linux/string.h>
+#else
+#include <string.h>
+#endif
+
+#endif
